9919a5f398d6b1165eb1cb583d364900bc4affd8
mahyarj <mahyar.jamalabadi@google.com>
2023-10-22 18:20:44 +0300

    Add socket.io

--- index.ts
+++ index.ts
@@ -1,15 +1,35 @@
 import express, { Express, Request, Response } from "express";
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
+const httpServer = createServer(app);
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server is running");
+const io = new Server(httpServer, {
+  cors: {
+    // Credential is not supported if the CORS header ‘Access-Control-Allow-Origin’ is "origin: *"
+    origin: "*",
+    // with a single string origin
+    // or with an array of origins
+    // origin: ["https://my-frontend.com", "https://my-other-frontend.com", "http://localhost:3000"],
+    // credentials: true,
+  },
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("⚡️[server]: Socket id: ", socket.id);
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + Socket.IO + TypeScript Server is running");
+});
+
+httpServer.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
