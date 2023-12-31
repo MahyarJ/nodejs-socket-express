dbc4c0a5b292c29faa0fb4ba25cd3a90ea94d603
mahyarj <mahyar.jamalabadi@google.com>
2023-12-31 12:17:16 +0200

    Add alphabet to sensor id

--- generateSensors.ts
+++ generateSensors.ts
@@ -1,13 +1,18 @@
 export type Sensor = {
   index: number;
-  id: number;
+  id: string;
 };
 
 const generateSensors = (length: number): Sensor[] => {
   return Array.from({ length }, (_, i) => {
+    // Generate a number between 65 (A) and 90 (Z)
+    const char = String.fromCharCode(
+      Math.floor(Math.random() * (90 - 65 + 1)) + 65
+    );
+    const num = Math.floor(Math.random() * 100);
     return {
       index: i,
-      id: Math.floor(Math.random() * 100),
+      id: `${char}${num}`,
     };
   });
 };
